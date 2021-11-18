a7dd6c1515ce2c133d0989e159af8a9f6a195885
sara3344 <secrethappy@naver.com>
2021-11-18 12:38:42 +0900

    user 내용 추가
    user 생성자 부분 추가

--- houseProject/src/main/java/user/User.java
+++ houseProject/src/main/java/user/User.java
@@ -1,33 +1,68 @@
 package user;
 
 public class User {
-    private String userID;
-    private String userPW;
-    private String userName;
-    private String userPhone;
-    
-    public String getUserID() {
-        return userID;
-    }
-    public void setUserID(String userID) {
-        this.userID = userID;
-    }
-    public String getUserPW() {
-        return userPW;
-    }
-    public void setUserPW(String userPW) {
-        this.userPW = userPW;
-    }
-    public String getUserName() {
-        return userName;
-    }
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-    public String getUserPhone() {
-        return userPhone;
-    }
-    public void setUserPhone(String userPhone) {
-        this.userPhone = userPhone;
-    }
+	private String userID;
+	private String userPW;
+	private String userName;
+	private String userPhone;
+	private String userType;
+	private String userWithdraw;
+	private String userMarketSelect;
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public String getUserPW() {
+		return userPW;
+	}
+
+	public void setUserPW(String userPW) {
+		this.userPW = userPW;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPhone() {
+		return userPhone;
+	}
+
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public String getUserWithdraw() {
+		return userWithdraw;
+	}
+
+	public void setUserWithdraw(String userWithdraw) {
+		this.userWithdraw = userWithdraw;
+	}
+
+	public String getUserMarketSelect() {
+		return userMarketSelect;
+	}
+
+	public void setUserMarketSelect(String userMarketSelect) {
+		this.userMarketSelect = userMarketSelect;
+	}
+
 }
