becc8ab7358c3b7ec911118059bdd1497a147028
Jenny <secrethappy@naver.com>
2021-11-22 15:10:17 +0900

    sql 수정
    -sql 내용 수정
    -user, userDAO, bbs, bbsDAO, comment, commentDAO 수정

--- houseProject/src/main/java/bbs/Bbs.java
+++ houseProject/src/main/java/bbs/Bbs.java
@@ -1,86 +1,104 @@
 package bbs;
 
 public class Bbs {
-	private int boardID; // 게시판 순번
-	private String boardTitle; // 게시글 제목
-	private String boardContent; // 게시글 내용
-	private String createrID; // 작성자 id
-	private String createDate; // 작성일
-	private String updaterID; // 수정자 id
-	private String updateDate; // 수정일
-	private int boardAvailable;
-	private int viewCount; // 조회수
+    private int boardID; // 게시판 순번
+    private String boardTitle; // 게시글 제목
+    private String boardContent; // 게시글 내용
+    private String createrID; // 작성자 id
+    private String createDate; // 작성일
+    private String updaterID; // 수정자 id
+    private String updateDate; // 수정일
+    private int viewCount; // 조회수
+    private int boardType; // 게시판 유형
+    private int boardAvailable; // 게시글 상태
+    private int secretCheck; // 비밀글
 
-	public int getBoardID() {
-		return boardID;
-	}
+    public int getBoardID() {
+        return boardID;
+    }
 
-	public void setBoardID(int boardID) {
-		this.boardID = boardID;
-	}
+    public void setBoardID(int boardID) {
+        this.boardID = boardID;
+    }
 
-	public String getBoardTitle() {
-		return boardTitle;
-	}
+    public String getBoardTitle() {
+        return boardTitle;
+    }
 
-	public void setBoardTitle(String boardTitle) {
-		this.boardTitle = boardTitle;
-	}
+    public void setBoardTitle(String boardTitle) {
+        this.boardTitle = boardTitle;
+    }
 
-	public String getBoardContent() {
-		return boardContent;
-	}
+    public String getBoardContent() {
+        return boardContent;
+    }
 
-	public void setBoardContent(String boardContent) {
-		this.boardContent = boardContent;
-	}
+    public void setBoardContent(String boardContent) {
+        this.boardContent = boardContent;
+    }
 
-	public String getCreaterID() {
-		return createrID;
-	}
+    public String getCreaterID() {
+        return createrID;
+    }
 
-	public void setCreaterID(String createrID) {
-		this.createrID = createrID;
-	}
+    public void setCreaterID(String createrID) {
+        this.createrID = createrID;
+    }
 
-	public String getCreateDate() {
-		return createDate;
-	}
+    public String getCreateDate() {
+        return createDate;
+    }
 
-	public void setCreateDate(String createDate) {
-		this.createDate = createDate;
-	}
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
 
-	public String getUpdaterID() {
-		return updaterID;
-	}
+    public String getUpdaterID() {
+        return updaterID;
+    }
 
-	public void setUpdaterID(String updaterID) {
-		this.updaterID = updaterID;
-	}
+    public void setUpdaterID(String updaterID) {
+        this.updaterID = updaterID;
+    }
 
-	public String getUpdateDate() {
-		return updateDate;
-	}
+    public String getUpdateDate() {
+        return updateDate;
+    }
 
-	public void setUpdateDate(String updateDate) {
-		this.updateDate = updateDate;
-	}
+    public void setUpdateDate(String updateDate) {
+        this.updateDate = updateDate;
+    }
 
-	public int getBoardAvailable() {
-		return boardAvailable;
-	}
+    public int getViewCount() {
+        return viewCount;
+    }
 
-	public void setBoardAvailable(int boardAvailable) {
-		this.boardAvailable = boardAvailable;
-	}
+    public void setViewCount(int viewCount) {
+        this.viewCount = viewCount;
+    }
 
-	public int getViewCount() {
-		return viewCount;
-	}
+    public int getBoardType() {
+        return boardType;
+    }
 
-	public void setViewCount(int viewCount) {
-		this.viewCount = viewCount;
-	}
+    public void setBoardType(int boardType) {
+        this.boardType = boardType;
+    }
+
+    public int getBoardAvailable() {
+        return boardAvailable;
+    }
+
+    public void setBoardAvailable(int boardAvailable) {
+        this.boardAvailable = boardAvailable;
+    }
+
+    public int getSecretCheck() {
+        return secretCheck;
+    }
+
+    public void setSecretCheck(int secretCheck) {
+        this.secretCheck = secretCheck;
+    }
 
 }
