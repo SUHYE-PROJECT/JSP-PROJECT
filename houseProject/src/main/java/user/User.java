280cdc2057a65564f8217186ef9ca9f6704d08d6
JensenKim <87356942+Jensenkim@users.noreply.github.com>
2021-11-23 15:16:57 +0900

    Revert "Revert "로그인 관련 세션""
    This reverts commit 6cd7eb1eec66cf5a2af0e0f8b2340a788c7bfd37.

--- houseProject/src/main/java/user/User.java
+++ houseProject/src/main/java/user/User.java
@@ -1,57 +1,68 @@
 package user;
 
 public class User {
-    private String userID;
-    private String userPW;
-    private String userName;
-    private String userPhone;
-    private int userType;
-    private int userWithdraw;
-    private int userMarketSelect;
-    
-    public String getUserID() {
-        return userID;
-    }
-    public void setUserID(String userID) {
-        this.userID = userID;
-    }
-    public String getUserPW() {
-        return userPW;
-    }
-    public void setUserPW(String userPW) {
-        this.userPW = userPW;
-    }
-    public String getUserName() {
-        return userName;
-    }
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-    public String getUserPhone() {
-        return userPhone;
-    }
-    public void setUserPhone(String userPhone) {
-        this.userPhone = userPhone;
-    }
-    public int getUserType() {
-        return userType;
-    }
-    public void setUserType(int userType) {
-        this.userType = userType;
-    }
-    public int getUserWithdraw() {
-        return userWithdraw;
-    }
-    public void setUserWithdraw(int userWithdraw) {
-        this.userWithdraw = userWithdraw;
-    }
-    public int getUserMarketSelect() {
-        return userMarketSelect;
-    }
-    public void setUserMarketSelect(int userMarketSelect) {
-        this.userMarketSelect = userMarketSelect;
-    }
+	private String userID; // ID
+	private String userPW; // 비밀번호
+	private String userName; // 이름
+	private String userPhone; // 전화번호
+	private int userType; // 관리자, 일반회원
+	private int userWithdraw; // 활동상태
+	private int userMarketSelect; // 수신 선택동의
 
+	public String getUserID() {
+		return userID;
+	}
 
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public String getUserPW() {
+		return userPW;
+	}
+
+	public void setUserPW(String userPW) {
+		this.userPW = userPW;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPhone() {
+		return userPhone;
+	}
+
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+
+	public int getUserType() {
+		return userType;
+	}
+
+	public void setUserType(int userType) {
+		this.userType = userType;
+	}
+
+	public int getUserWithdraw() {
+		return userWithdraw;
+	}
+
+	public void setUserWithdraw(int userWithdraw) {
+		this.userWithdraw = userWithdraw;
+	}
+
+	public int getUserMarketSelect() {
+		return userMarketSelect;
+	}
+
+	public void setUserMarketSelect(int userMarketSelect) {
+		this.userMarketSelect = userMarketSelect;
+	}
 
 }
--- houseProject/src/main/webapp/session.jsp
+++ houseProject/src/main/webapp/session.jsp
@@ -6,5 +6,6 @@ String userID = null;
 if (session.getAttribute("userID") != null) { //세션의 userID속성이 null이 아니면
     userID = (String) session.getAttribute("userID");
 } else { //현재 세션의 userID가 null이면
+	
 }
 %>
